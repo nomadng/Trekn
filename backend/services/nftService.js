3afa17b195e495184231cf172438a9e7d4c5b33c
onspeedhp <chaukhac4@gmail.com>
2023-08-17 19:57:43 +0700

    Update lib

--- backend/services/nftService.js
+++ backend/services/nftService.js
@@ -1,25 +1,27 @@
 import { randomRarityNFT } from '@root/utils/randomRarity'
-import { PublicKey, Keypair } from '@solana/web3.js'
-import { createTrxMintCompressedNft } from '../utils/mintcNFT'
-import { createMintcNFTProps } from '../utils/createMintcNFTProps'
+import { createTrxMintCompressedNft } from '@root/utils/mintcNFT'
+import { createMintcNFTProps } from '@root/utils/createMintcNFTProps'
 
 export const mintNft = async (req) => {
   const { address, locationId, userPubkey } = req.body
 
   // Step 1: Random rarity
   const rarity = randomRarityNFT()
-  const treeAddressString = new PublicKey('')
-  const collectionMintString = new PublicKey('')
-  const collectionMetadataAccountString = new PublicKey('')
+
+  const treeAddressString = ''
+  const collectionMintString = ''
+  const collectionMetadataAccountString = ''
+  const collectionMasterEditionAccountString = ''
 
   const base64Transaction = await createTrxMintCompressedNft(
-    createMintcNFTProps({
-      userPubkeyString: userPubkey,
-      treeAddressString: treeAddressString,
-      collectionMintString: collectionMintString,
-      collectionMetadataAccountString: collectionMetadataAccountString,
-    })
+    createMintcNFTProps(
+      userPubkey,
+      treeAddressString,
+      collectionMintString,
+      collectionMetadataAccountString,
+      collectionMasterEditionAccountString
+    )
   )
 
-  res.send({ transaction: base64Transaction })
+  // res.send({ transaction: base64Transaction })
 }
