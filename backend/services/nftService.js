151f2a0db7f38e0c782ce2d3b0efee7039d5668b
onspeedhp <chaukhac4@gmail.com>
2023-08-17 20:03:46 +0700

    Update nftService

--- backend/services/nftService.js
+++ backend/services/nftService.js
@@ -1,10 +1,23 @@
 import { randomRarityNFT } from '@root/utils/randomRarity'
-import { createTrxMintCompressedNft } from '@root/utils/mintcNFT'
-import { createMintcNFTProps } from '@root/utils/createMintcNFTProps'
+import { createTrxMintCompressedNft } from '../utils/mintcNFT'
+import { createMintcNFTProps } from '../utils/createMintcNFTProps'
 
 export const mintNft = async (req) => {
   const { address, locationId, userPubkey } = req.body
 
   // Step 1: Random rarity
   const rarity = randomRarityNFT()
+  const treeAddressString = ''
+  const collectionMintString = ''
+  const collectionMetadataAccountString = ''
+  const collectionMasterEditionAccountString = ''
+  const base64Transaction = await createTrxMintCompressedNft(
+    createMintcNFTProps({
+      userPubkey,
+      treeAddressString,
+      collectionMintString,
+      collectionMetadataAccountString,
+      collectionMasterEditionAccountString,
+    })
+  )
 }
