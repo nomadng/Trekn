5f5f83a93413d1a36c767ba9167b5e1d520113d2
datnt <datnt@pando-infinity.com>
2023-08-17 19:38:12 +0700

    seed db

--- backend/seeder/seeder.js
+++ backend/seeder/seeder.js
@@ -1,12 +1,12 @@
 import { locationSeeder } from '@root/seeder/locationSeeder';
 import { nationSeeder } from '@root/seeder/nationSeeder';
 import { locationPhotoSeeder } from '@root/seeder/locationPhotoSeeder';
-import {collectionSeeder} from "@root/seeder/collectionSeeder";
+import { collectionSeeder } from '@root/seeder/collectionSeeder';
 
 const seeder = async () => {
-  // await nationSeeder()
-  // await locationSeeder()
-  // await locationPhotoSeeder()
+  await nationSeeder()
+  await locationSeeder()
+  await locationPhotoSeeder()
   await collectionSeeder()
 }
 
