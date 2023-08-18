d7cc93b6926c6d0708bd7fa9aa397eeee2dbc44d
DDS-NamHT <83576072+DDS-NamHT@users.noreply.github.com>
2023-08-18 12:54:25 +0700

    add logic check exists document

--- backend/services/nftService.js
+++ backend/services/nftService.js
@@ -1,7 +1,9 @@
 import { randomRarityNFT } from '@root/utils/randomRarity'
 import { randomLocationPhotoByConditions } from '@root/repositories/locationPhotoRepository'
-import { NFT_ATTRIBUTE_SEASON } from '@root/utils/constants'
+import { HTTP_CONSTANTS, NFT_ATTRIBUTE_SEASON } from '@root/utils/constants'
 import { findLocationById } from '@root/repositories/locationRepository'
+import { LOCATION_NOT_FOUND, LOCATION_PHOTO_NOT_FOUND } from '@root/utils/responseMsg'
+import { BaseError } from '@root/utils/baseError'
 
 export const mintNft = async (req) => {
   const { address, locationId } = req.body
@@ -12,7 +14,17 @@ export const mintNft = async (req) => {
     rarity,
     locationId,
   })
+  if (!locationPhotoInfo) {
+    throw new BaseError(
+      new Error(LOCATION_PHOTO_NOT_FOUND),
+      LOCATION_PHOTO_NOT_FOUND,
+      HTTP_CONSTANTS.HTTP_STATUS_BAD_REQUEST
+    )
+  }
   const locationInfo = await findLocationById(locationId)
+  if (!locationInfo) {
+    throw new BaseError(new Error(LOCATION_NOT_FOUND), LOCATION_NOT_FOUND, HTTP_CONSTANTS.HTTP_STATUS_BAD_REQUEST)
+  }
 
   return {
     name: locationInfo.name,
